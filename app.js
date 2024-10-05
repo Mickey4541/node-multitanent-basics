9d6977c1f86c06354ff95d26f9450a2189f10c1f
mickey4541 <rajanbhandari4541@gmail.com>
2024-10-05 17:08:28 +0545

    learned websocket and how it works

--- app.js
+++ app.js
@@ -4,22 +4,48 @@ const app = express()
 
 const userRoute = require("./route/userRoute")
 const organizationRoute = require("./route/organizationRoute")
+const { Server, Socket } = require("socket.io")
+const { users } = require("./model/index")
 
 //requiring database
 require("./model/index")
 
+app.set("view engine", "ejs")
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(require('cookie-parser')())
 
 app.get("/",(req,res)=>{
-    res.send("I am alive")
+    res.render("home")
 })
 
 app.use("",userRoute)
 app.use("",organizationRoute)
 
-app.listen(3000,()=>{
+const server = app.listen(3000,()=>{
     console.log("Nodejs has started at port 3000");
+})
+
+//configuring socket.io
+const io = new Server(server)
+
+//emit vaneko pathauni on vaneko listen garni
+io.on("connection", (socket)=>{
+    socket.on("register",async(data)=>{
+        console.log(data);
+        const { username, password, email } = data;
+        try {
+            await users.create({ username, password, email });
+            socket.emit("response", "Registration successful");
+        } catch (error) {
+            console.error(error);
+            socket.emit("response", "Registration failed");
+        }
+    // console.log("A user connected");
+    // socket.on("hello", (data)=>{
+    //     console.log(data);
+    //     socket.emit("response", "Data was received in backend")
+    // })
+})
 })
